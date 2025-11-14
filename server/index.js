3d74036b6a8b16a65d8f6e3eeee21184bad39c48
Phantasmal24 <aditykr241106@gmail.com>
2025-11-15 01:14:23 +0530

    Feat: Project Working

--- server/index.js
+++ server/index.js
@@ -2,13 +2,12 @@
 // and must be required before other modules
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');  // 'cors' is scurity middleware for Express
+const cors = require('cors');  // 'cors' is scurity middleware for Express
 
 // -- Importing the route files --
 const trendingRoutes = require('./routes/trending');
 const genreRoutes = require('./routes/genres');
 const platformRoutes = require('./routes/platforms');
-const popularRoutes = require('./routes/popular'); // <-- ADD THIS NEW LINE
 
 // Creating the main Express applications
 const app = express();
@@ -16,21 +15,28 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
+// we use 'app.use' to add middleware on every request
+/**
+ * WHY: 'cors' allows out React app to make request to this server (running on localHost: 3001). Without it, the browser would blick the request for security reasons
+ */
 app.use(cors());
+// This allows us to parse JSON data from the request body
 app.use(express.json());
 
 // -- Using the route files --
+// For passing any request that comes on '/api' to the trendingRoutes router to see if it matches
 app.use('/api', trendingRoutes);
+// For passing any request that comes on '/api' to the genreRoutes router to see if it matches
 app.use('/api', genreRoutes);
+// For passing any request that comes on '/api' to the platformRoutes router to see if it matches
 app.use('/api', platformRoutes);
-app.use('/api', popularRoutes); // <-- ADD THIS NEW LINE
 
 // Starting the server
+// This line will start the server and listen on port 3001, and will log a message to the console when it starts
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  	console.log('You can now test your endpoints in Postman.');
-  	console.log('GET http://localhost:3001/api/trending (LIVE Twitch data)');
-    console.log('GET http://localhost:3001/api/popular (Random All-Time data)');
-  	console.log('GET http://localhost:3001/api/genre-trends');
-  	console.log('GET http://localhost:3001/api/platform-performance');
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log('You can now test your endpoints in Postman.');
+    console.log('GET http://localhost:3001/api/trending');
+    console.log('GET http://localhost:3001/api/genre-trends');
+    console.log('GET http://localhost:3001/api/platform-performance');
 });
